f42e1c6badab217dce2cb8033c8071d25686b5d1
stivasquez09 <stivasquez09@gmail.com>
2026-01-10 20:30:30 -0500

    readme para modulo route53

--- Route53/main.tf
+++ Route53/main.tf
@@ -17,14 +17,11 @@ resource "aws_route53_zone" "this" {
 
 
 resource "aws_route53_record" "records" {
-  for_each = { for r in var.records : r.name => r }
+  for_each = { for r in var.records : "${r.name}_${r.type}" => r }
 
   zone_id = aws_route53_zone.this.zone_id
   name    = each.value.name
   type    = each.value.type
-
-  ttl     = lookup(each.value, "ttl", null)
-  records = lookup(each.value, "records", null)
 
   dynamic "alias" {
     for_each = lookup(each.value, "alias", null) == null ? [] : [each.value.alias]
@@ -32,6 +29,20 @@ resource "aws_route53_record" "records" {
       name                   = alias.value.name
       zone_id                = alias.value.zone_id
       evaluate_target_health = lookup(alias.value, "evaluate_target_health", false)
+    }
+  }
+
+  dynamic "ttl" {
+    for_each = lookup(each.value, "alias", null) == null ? [1] : []
+    content {
+      ttl = lookup(each.value, "ttl", 300)
+    }
+  }
+
+  dynamic "records" {
+    for_each = lookup(each.value, "alias", null) == null ? [1] : []
+    content {
+      records = lookup(each.value, "records", [])
     }
   }
 }
